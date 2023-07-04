d04c6d85a6163d72fc7ad952cd103354b4d7c93a
shakyevan444 <84460058+shakyevan444@users.noreply.github.com>
2023-07-03 23:53:19 -0400

    Update pre-boot.js
    added extra comments

--- pre-boot.js
+++ pre-boot.js
@@ -1,18 +1,21 @@
 
 //Pre-Boot
 
-var Version = "1.0";
-var Codename = "Cupcake";
-var UIVersion = "1.0";
+var Version = "1.0"; //set version
+var Codename = "Cupcake"; //set version codename
+var UIVersion = "1.0"; //set NUI version
+//show versions in console and set them in OS
 console.log("Version " + Version);
 console.log("UI Version " + UIVersion);
 setText("app.settings.text.2", "NUI " + UIVersion);
 setText("app.settings.text.1", "Nexa "  + Codename);
+//if enabled, run CheckBIOS
 getKeyValue("6", function (value) {
   if (value == 1) {
     CheckBIOS();
   }
 });
+//function that checks security of if this is a legit device or not
 function CheckSec() {
   getKeyValue("1", function (v1) {
     getKeyValue("2", function (v2) {
