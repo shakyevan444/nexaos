4145fbc395fbd353fa900a0a6c3de42a31d242de
shakyevan444 <84460058+shakyevan444@users.noreply.github.com>
2023-07-04 00:05:51 -0400

    Update after-boot.js

--- after-boot.js
+++ after-boot.js
@@ -3,11 +3,11 @@
 
 //Setup
 //login
-onEvent("root.login.button", "click", function( ) {
+onEvent("root.login.button", "click", function( ) { //login code
   setScreen("Homescreen.1");
   CheckSec();
 });
-//Notifications/logout
+//code for Notifications and logging-out
 function Logout() {
   setProperty("Homescreen.1", "image", "800w-98aZLNQxJXg.png");
   setProperty("app.appstore.1", "image", "800w-98aZLNQxJXg.png");
@@ -32,7 +32,7 @@ onEvent("Homescreen.notif.button.1", "click", function( ) {
 onEvent("Homebar.13.3", "click", function( ) {
   setScreen("Homescreen.1");
 });
-//Homescreen
+//Homescreen Code
 onEvent("Homebar.1.1", "click", function( ) {
   setScreen("Homescreen.notif");
 });
@@ -60,41 +60,41 @@ onEvent("Homescreen.1.app.appstore", "click", function( ) {
 onEvent("Homescreen.1.app.tips", "click", function( ) {
   setScreen("app.tips");
 });
-//app.settings
-function CheckBIOS() {
+//code for app.settings (settings)
+function CheckBIOS() { //sets what CheckBIOS does (currently nothing)
   
 }
 onEvent("Homebar.2.2", "click", function( ) {
   setScreen("Homescreen.1");
 });
-onEvent("app.settings.button.7", "click", function( ) {
+onEvent("app.settings.button.7", "click", function( ) { //goto the wallpaper picker
   setScreen("app.settings.customizer");
   CheckSec();
 });
-onEvent("app.settings.button.8", "click", function( ) {
+onEvent("app.settings.button.8", "click", function( ) { //goto info screen
   setScreen("app.settings.info");
 });
-//app.settings.info
+//code for app.settings.info (info page)
 onEvent("Homebar.14.1", "click", function( ) {
   setScreen("app.settings");
 });
 setText("app.settings.info.text.1", "NexaOS Version " + Version);
 setText("app.settings.info.text.2", "Codename " + Codename);
 setText("app.settings.info.text.3", "NexusUI Version " + UIVersion);
-//app.settings.login
+//code for app.settings.login (login to acc page)
 onEvent("app.settings.button.6", "click", function( ) {
   setScreen("app.settings.login");
   CheckSec();
 });
-onEvent("app.settings.login.button.2", "click", function( ) { 
-  if (getText("app.settings.login.input.1") == getColumn(getText("app.settings.login.input.1"), "Username")) {
+onEvent("app.settings.login.button.2", "click", function( ) { //make acc
+  if (getText("app.settings.login.input.1") == getColumn(getText("app.settings.login.input.1"), "Username")) { //check if account exists
     console.log("Account already exists");
     setText("app.settings.login.text.1", "Account already exists");
     showElement("app.settings.login.text.1");
     setTimeout(function() {
       hideElement("app.settings.login.text.1");
     }, 1000);
-  } else {
+  } else { //make the acc
     createRecord(getText("app.settings.login.input.1"), {terminated: 0, dev: 0, USERID: (getUserId()), Wallpaper:0, Username:(getText("app.settings.login.input.1")), Password:(getText("app.settings.login.input.2")), }, function(record) {
       
     });
@@ -106,10 +106,10 @@ var Username = "NULL";
 onEvent("app.settings.login.button.1", "click", function( ) {
   readRecords(getText("app.settings.login.input.1"), {}, function(records) {
     for (var i =0; i < records.length; i++) {
-      if((records[i]).Password == getText("app.settings.login.input.2") ){
-       if ((records[i]).dev == 1) {
+      if((records[i]).Password == getText("app.settings.login.input.2") ){ //check if the password is correct
+       if ((records[i]).dev == 1) { //check if acc is a dev
          CheckSec();
-         if ((records[i]).terminated == 0) {
+         if ((records[i]).terminated == 0) { //check if acc is terminated
            dev = 1;
            showElement("app.appstore.1.app.dev");
            Username = records.Username;
@@ -124,7 +124,7 @@ onEvent("app.settings.login.button.1", "click", function( ) {
            }, 1000);
          }
        } else {
-         if (records.terminated == 0) {
+         if (records.terminated == 0) { //check if acc is terminated
            if((records[i]).Password == getText("app.settings.login.input.2") ){
             dev = 0;
             Username = records.Username;
@@ -154,7 +154,7 @@ onEvent("app.settings.login.button.1", "click", function( ) {
       }
   });
 });
-//app.settings.customizer
+//code for app.settings.customizer (wallpapers)
 onEvent("Homebar.9.1", "click", function( ) {
   setScreen("app.settings");
 });
@@ -209,7 +209,7 @@ function Seasonalwallpaper() {
   });
 }
 Seasonalwallpaper();
-//app.appstore.1
+//code for app.appstore.1 (appstore page 1)
 onEvent("Homebar.3.2", "click", function( ) {
   setScreen("Homescreen.1");
 });
@@ -261,8 +261,8 @@ onEvent("app.appstore.1.app.notes", "click", function( ) {
   showElement("Homescreen.1.app.notes");
   showElement("app.appstore.1.app.notes.delete");
 });
-//app.music
-function stopmusic() {
+//code for app.music (music)
+function stopmusic() { //func to stop all music 
   CheckSec();
   stopSound("Beluga---Belupacito-(Official-Audio).mp3");
   stopSound("L_X.mp3");
@@ -324,49 +324,49 @@ onEvent("app.music.button.1", "click", function( ) {
 onEvent("Homescreen.1.widgetbar.1", "click", function( ) {
   stopmusic();
 });
-//app.tips
+//code for app.tips (tips)
 onEvent("Homebar.7.2", "click", function( ) {
   setScreen("Homescreen.1");
 });
-//app.dev
+//code for app.dev (dev)
 onEvent("Homebar.4.2", "click", function( ) {
   setScreen("Homescreen.1");
 });
 onEvent("app.dev.button.1", "click", function( ) {
   setScreen("root.terminal");
 });
-//app.dev.terminal
+//code for app.dev.terminal (terminal)
 onEvent("run", "click", function( ) {
-  if (getText("command") == "test" ) {
+  if (getText("command") == "test" ) { //test cmd, replys "Test!"
     setText("text_area3", "Test! / >");
   } else {
-    if (getText("command") == "setscr" ) {
+    if (getText("command") == "setscr" ) { //set screen to param1
       setText("text_area3", "/ >");
       setScreen(getText("param1"));
     } else {
-      if (getText("command") == "settext" ) {
+      if (getText("command") == "settext" ) { //set element param1 text to param2
         setText(getText("param1"), getText("param2"));
       } else {
-        if (getText("command") == "print" ) {
+        if (getText("command") == "print" ) { //prints param1 to terminal
           setText("text_area3", getText("param1") + " / >");
         } else {
-          if (getText("command") == "version" ) {
+          if (getText("command") == "version" ) { //print version
             setText("text_area3", Version + " / >");
           } else {
-            if (getText("command") == "home" ) {
+            if (getText("command") == "home" ) { //goes to root.login
               setScreen("root.login");
             } else {
-              if (getText("command") == "recovery" ) {
+              if (getText("command") == "recovery" ) { //goes to recovery menu (not in NexaOS)
                 setScreen("root.recovery");
               } else {
-                if (getText("command") == "oos" ) {
+                if (getText("command") == "oos" ) { //Other OS code
                   setText("text_area3", "Invalid Command! / >");
-                  getKeyValue("oos" + getText("param1"), function (val1) {
-                    getKeyValue("oosi" + getText("param1"), function (val2) {
-                      if (val2 == 1) {
-                        open(val1);
+                  getKeyValue("oos" + getText("param1"), function (val1) { //"oos{OOS NAME}" is the link to the other OS
+                    getKeyValue("oosi" + getText("param1"), function (val2) { //check if the oos is a link or page
+                      if (val2 == 1) { //if its a page go to that page
+                        open(val1); //opens the link
                       } else {
-                        setScreen(val1);
+                        setScreen(val1); //opens the page
                       }
                     });
                   });
@@ -381,24 +381,24 @@ onEvent("run", "click", function( ) {
     }
   }
 });
-//app.paint
-setActiveCanvas("app.paint.canvas");
+//code for app.paint (paint)
+setActiveCanvas("app.paint.canvas"); //set the canvas to paint
 var currentColor = "black";
 var isDragging = false;
 var previousX, previousY;
-onEvent("app.paint.red", "input", function(event) {
+onEvent("app.paint.red", "input", function(event) { //code for red slider
   var redValue = getProperty("app.paint.red", "value");
   updateColor(redValue, null, null);
 });
-onEvent("app.paint.green", "input", function(event) {
+onEvent("app.paint.green", "input", function(event) { //code for green slider
   var greenValue = getProperty("app.paint.green", "value");
   updateColor(null, greenValue, null);
 });
-onEvent("app.paint.blue", "input", function(event) {
+onEvent("app.paint.blue", "input", function(event) { //code for blue slider
   var blueValue = getProperty("app.paint.blue", "value");
   updateColor(null, null, blueValue);
 });
-onEvent("app.paint.canvas", "mousemove", function(event) {
+onEvent("app.paint.canvas", "mousemove", function(event) { //if your clicking on the canvas, paint on it
   if (isDragging) {
     var x = event.x - getXPosition("app.paint.canvas");
     var y = event.y - getYPosition("app.paint.canvas");
@@ -409,7 +409,7 @@ onEvent("app.paint.canvas", "mousemove", function(event) {
     previousY = y;
   }
 });
-onEvent("app.paint.canvas", "mousedown", function(event) {
+onEvent("app.paint.canvas", "mousedown", function(event) { //check if you are clicking/dragging
   isDragging = true;
   previousX = event.x - getXPosition("app.paint.canvas");
   previousY = event.y - getYPosition("app.paint.canvas");
@@ -417,18 +417,18 @@ onEvent("app.paint.canvas", "mousedown", function(event) {
 onEvent("app.paint.canvas", "mouseup", function() {
   isDragging = false;
 });
-onEvent("app.paint.button.clear", "click", function() {
+onEvent("app.paint.button.clear", "click", function() { //clear paint canvas
   clearCanvas("app.paint.canvas");
 });
-function paintRect(x, y) {
+function paintRect(x, y) { //unused function that draws a rectangle
   rect(x, y, 10, 10);
 }
-function drawLine(x1, y1, x2, y2, lineWidth) {
+function drawLine(x1, y1, x2, y2, lineWidth) { //draw function
   setStrokeWidth(lineWidth);
   setStrokeColor(currentColor);
   line(x1, y1, x2, y2);
 }
-function updateColor(red, green, blue) {
+function updateColor(red, green, blue) { //update color function
   red = getNumber("app.paint.red");
   green = getNumber("app.paint.green");
   blue = getNumber("app.paint.blue");
@@ -437,12 +437,12 @@ function updateColor(red, green, blue) {
 onEvent("Homebar.8.2", "click", function( ) {
   setScreen("Homescreen.1");
 });
-//app.notes
+//code for app.notes (notes)
 CheckSec();
 onEvent("Homebar.6.2", "click", function( ) {
   setScreen("Homescreen.1");
 });
-//app.phone
+//code for app.phone (phone)
 var PI = "";
 onEvent("Homebar.11.2", "click", function( ) {
   setScreen("Homescreen.1");
@@ -489,20 +489,20 @@ onEvent("app.phone.button.10", "click", function( ) {
 });
 onEvent("app.phone.button.11", "click", function( ) {
   getKeyValue("uc", function (value) {
-    if (PI == "459") {
+    if (PI == "459") { //easter egg from EvanOS
       console.log("EvanOS activated");
       PI = "";
-      setText("app.phone.text.2", PI);
+      setText("app.phone.text.2", PI); //easter egg from an old arcane os app
     } else if ((PI == "95632")) {
       console.log("Purchase Completed: Etunes");
       PI = "";
       setText("app.phone.text.2", PI);
-    } else if ((PI == value)) {
+    } else if ((PI == value)) { //enabled appstore when NexaOS was in beta
       setScreen("app.appstore.1");
       PI = "";
       setText("app.phone.text.2", PI);
     } else {
-      PI = "";
+      PI = ""; //reset input for phone when called
       setText("app.phone.text.2", PI);
     }
   });
